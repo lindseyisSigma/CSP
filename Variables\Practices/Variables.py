025deab3f04e1aed1abfb8555059ea6239dff462
lindseyisSigma <lindsey.mendoza@ucas-edu.net>
2024-09-06 19:33:29 -0700

    Update Variables.py

--- Variables\Practices/Variables.py
+++ Variables\Practices/Variables.py
@@ -1,5 +1,20 @@
 name="Lindsey"
-
-print (name)
+  print (name)
 smallNumbers= "6"
-print (smallNumbers)
+  print (smallNumbers)
+bigNumbers="795"
+  print(bigNumbers)
+breakFast="Bacon and Eggs"
+  print(breakFast)
+favoriteColor="Blue"
+  print(favoriteColor)
+school="UCAS"
+  print(school)
+year="2005"
+  print(year)
+eyeColor="Dark Brown"
+  print(eyeColor)
+myAge="14"
+  print(myAge)
+favoriteSubject="English"
+  print(favoriteSubject)
