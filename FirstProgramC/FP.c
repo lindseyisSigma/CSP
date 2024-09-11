0a70bee89715e936596534d50bcf4be17617f5eb
lindseyisSigma <lindsey.mendoza@ucas-edu.net>
2024-09-11 19:35:22 +0000

    made changes to setting

--- FirstProgramC/FP.c
+++ FirstProgramC/FP.c
@@ -1,9 +1,9 @@
-#include <stdio.h>.h>
+#include <stdio.h>
 
 int main(void) {
     char favoriteSport[30];
     printf("What is your favorite sport: \n");
     fgets(favoriteSport, sizeof(favoriteSport), stdin);
-    printf("Cool! \n");
+    printf("Cool!");
     return 0;
 }
