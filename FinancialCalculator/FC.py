56deb66b17f6c1e2118f3c7bd8c440e4f20c1867
lindseyisSigma <lindsey.mendoza@ucas-edu.net>
2024-09-20 22:19:11 +0000

    Made new changes to FC for py

--- FinancialCalculator/FC.py
+++ FinancialCalculator/FC.py
@@ -4,13 +4,28 @@ utils = float(input("What is your monthly utilities: \n"))
 groceries = float(input("What is your monthly groceries: \n"))
 transport = float(input("What is your monthly transportation costs: \n"))
 
-spent = rent + utils + groceries + transport;
-savings = income * .2;
-budget = income - spent - savings;
-print(budget);
+spent = rent + utils + groceries + transport; 
+savings = income * .2; 
+budget = income - spent - savings; 
+print(budget); 
 
-print("In percent that is. \n");
-prent = int(rent /income *100);
-putils
-pgroceries
-ptransport
+print("In percent that is. \n"); 
+prent = int(rent /income *100); 
+putils = int(utils/income *100); 
+pgroceries = int(groceries /income *100); 
+ptransport = int(transport /income*100); 
+
+print(prent, "%\n"); 
+print(putils, "% \n"); 
+print(pgroceries, "% \n"); 
+print(ptransport, "% \n"); 
+
+print("In total you spend \n"); 
+pspent = int(spent /income *100); 
+print(pspent, "% \n"); 
+
+print("You save 20% \n"); 
+
+print("Your budget is: "); 
+pbudget = int(budget /income *100);
+print(pbudget, "% \n"); 
---
+++ Strings/tempCodeRunnerFile.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+    char animal [20]; 
+    char place [20];
+    char verb [20];
+    char sentence[400] ="The ";
+    printf("Name an animal:");
+    scanf("%s", animal);
+    printf("Name a place: ");
+    scanf("%s", place);
+    printf("Name a past tense verb: ");
+    scanf("%s", verb);
+    strcat(sentence, animal);
+    strcat(sentence, " went to the ");
+    strcat(sentence, place);
+    strcat(sentence, " and ");
+    strcat(sentence, verb);
+    strcat(sentence, " with his friend the angry turtle. \n");
+    printf("%s", sentence);
+    return 0;
+}
