6485838aaddc5ff9e070ed23949e2b920d3a72ab
lindseyisSigma <lindsey.mendoza@ucas-edu.net>
2024-09-23 19:14:54 +0000

    ND for python

--- FinancialCalculator/FC.c
+++ FinancialCalculator/FC.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int main(void){
-    float income, rent, utilities, groceries, transportation, savings, expenses, total;
+    float income, rent, utilities, groceries, transportation, savings, expenses, spend;
+    float prent, putilities, pgroceries, ptransportation, p savings, pexpenses
     printf("Hello and welcome to our budget calculator\n");
     printf("How much do you make a month?\n")
     scanf("%f", &income)
