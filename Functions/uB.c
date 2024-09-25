999d5a779dc13ba1f5e4088e764f6765d902de26
lindseyisSigma <lindsey.mendoza@ucas-edu.net>
2024-09-25 19:43:37 +0000

    heh

---
+++ Functions/uB.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+float income, rent, utilities, grocieries, transportation, savings, expenses, spend;
+
+float input (char type[], float var){
+    printf("Monthly %s:\n",type);
+    scanf( "%f" ,&var);
+    return var;
+}
+
+
+void percent(char type[], int amount){
+    int per = amount/income *100;
+
+    printf("Your %s is %d%% of your income.\n" , type, per);
+}
+
+
+int main(void){
+printf("This is a budget calculator. \n");
+income = input("income",income);
+rent = input("rent",rent);
+ = input("uncine",income);
+income = input("uncine",income);
+income = input("uncine",income);
+income = input("uncine",income);
+income = input("uncine",income);
+income = input("uncine",income);
+input("income", imcome);
+
+savings = income * .2;
+expenses = rent + utilities +grocieries +transportation;
+spend = income - expenses - savings;
+printf("You make $%.2f\n" , income);
+printf("Your expenses are $%.2f\n" , expenses);
+printf("Your savings are $%.2f\n" , savings);
+printf("Your spending money is $%.2f\n", spend);
+
+percent("rent", rent);
+percent("utilities", utilities);
+percent("grocieries", grocieries);
+percent("transportation", transportation);
+percent("expenses", expenses);
+percent("savings", savings);
+percent("spending money", spend);
+
+return 0;
+}
