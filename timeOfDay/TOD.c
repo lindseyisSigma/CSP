67094ec72f47a72b55162506540aafda87b0dc8d
lindseyisSigma <lindsey.mendoza@ucas-edu.net>
2024-10-03 19:25:36 +0000

    made new changes to TOD c

--- timeOfDay/TOD.c
+++ timeOfDay/TOD.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 #include <time.h>
+
 int hour;
+
 int main ()
 {
-    time_t rawtime;
-    struct tm * timeinfo;
+    time_t now;
+    struct tm * now_tm;
 
-    time (&rawtime);
-    timeinfo = localtime (&rawtime);
-    printf("Current local time and date: %s", asctime(timeinfo));
-    printf("Tell me the hour in military time:\n");
-    scanf("%d", &hour);
+    now = time(NULL);
+    now_tm = localtime(&now);
+    hour = (now_tm->tm_hour);
+    printf("%d\n", hour);
+    
     if (hour < 12){
         printf("Good Morning\n");
     }else if (hour < 18){
         printf("Good Afternoon\n");
     }else{
         printf("Good Good Evening\n");
-    }else{
-
-    }
+    };
     return 0;
 }
