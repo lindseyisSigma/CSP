186f387fc79ad087350f3d6c296fb22dbfa9edef
lindseyisSigma <lindsey.mendoza@ucas-edu.net>
2024-10-11 19:43:01 +0000

    tehehe

--- Loops/FIZBUZ.c
+++ Loops/FIZBUZ.c
@@ -9,10 +9,10 @@ int main(void){
     printf("Give me a number ");
     scanf("%d", &usr);
     printf("Give me a short word!\n");
-    scanf("%s", &one);
+    scanf("%s", one);
     printf("Give me a short word!\n");
-    scanf("%s", &two);
-    strcar(three, one);
+    scanf("%s", two);
+    strcat(three, one);
     strcat(three, two);
     //loop that counts to 50
     for(i=1;i<=usr;i++){
@@ -20,12 +20,12 @@ int main(void){
         //replace #'s diisible by 3 and 5 with "FizzBuzz"
          if(i%3==0 && i%5==0){
             printf("%s\n", three);
-        
+        s
         //replace #'s divisible by 3 with "Fizz"
             printf("%s\n", two);
-         } else if (i%5==0){
+         } else if (i%5==0){s
         //replace #'s dividible bt 5 with "Buzz"
-            printf("Buzz\n");
+            printf("%d\n");
          }else {
         //print the number
             printf("%d\n",i);
