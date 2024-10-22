c485664fd999cd879ef11958390e8fe35e7dd517
lindseyisSigma <lindsey.mendoza@ucas-edu.net>
2024-10-22 19:40:40 +0000

    tod oy

--- Conditionals/timeOfDay/TOD.py
+++ Conditionals/timeOfDay/TOD.py
@@ -3,3 +3,12 @@ import datetime
 currenttime = datetime.datetime.now()
 
 print(currenttime.hour)
+
+hour = 13
+
+if hour < 12:
+    print("Good Morning!")
+elif hour < 15:
+    print("Good Afternoon!")
+else: 
+    print("Good Evening!")
