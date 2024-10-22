38a9a87a4257afd78d012e6e9dd0997a5b9a3c7b
lindseyisSigma <lindsey.mendoza@ucas-edu.net>
2024-10-22 19:08:49 +0000

    OE for c and py

---
+++ Conditionals/OE.py
@@ -0,0 +1,11 @@
+age = 4
+if age >= 18:
+    print("You are old enough to vote!")
+elif age >= 16:
+    print("You are old enough to drive!")
+elif age >= 15:
+    print("You are old enough to get your learners permit!")
+elif age >= 5:
+    print("YOu are old enough to go to school!")
+else: 
+    print("You are too young to go to school")
