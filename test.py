18e6e4fbcb8cf82e33d503521eeea5ab0298ddf7
lindseyisSigma <lindsey.mendoza@ucas-edu.net>
2024-10-25 00:51:46 +0000

    made new changes tfor loops project

--- test.py
+++ test.py
@@ -1,4 +1,13 @@
-siblings = ["Alex", "Katie", "Andrew", "Viena", "Tia", "Treyson", "Xavier", "Jake"]
+gold = ["1", "2", "3"]
+gold: list[str]
+for gold in gold:
+    print(f"You now have {gold}!")
+Golds = 3
+text = "Gold" + str(Golds)
 
-for x in range(1,25):
-    print("Be quiet")
+live_remaning = ["1", "2", "3"]
+live_remaning: list[str]
+for live_remaning in live_remaning:
+    print(f"You now have {live_remaning}HP!")
+lives_remaning = 3
+text = "Lives" + str(lives_remaning)
